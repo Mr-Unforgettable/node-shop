e8087280efee0db2d126cb0bda70f9308cb862f4
Mr-Unforgettable <abhinavpsingh2024@protonmail.com>
2024-11-22 18:57:13 +0530

    Keeping StrictMode fragment for debugging in dev build.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
