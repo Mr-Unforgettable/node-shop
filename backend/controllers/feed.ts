f0ba82bfcff0e5b4c350d5727505d894fd5ec38c
Mr-Unforgettable <abhinavpsingh2024@protonmail.com>
2024-11-25 16:01:32 +0530

    Added server-side error handling logic

--- backend/controllers/feed.ts
+++ backend/controllers/feed.ts
@@ -2,6 +2,17 @@ import { RequestHandler } from "express";
 import { validationResult } from "express-validator";
 import { Post } from "../models/post";
 
+// Custom HttpError class
+class HttpError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Object.setPrototypeOf(this, HttpError.prototype);
+  }
+}
+
 export const getPosts: RequestHandler = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -23,33 +34,36 @@ export const createPost: RequestHandler = async (req, res, next) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(422).json({
-        message: "Validation failed, entered data is incorrect.",
-        errors: errors.array(),
+      const validationError = new HttpError(
+        "Validation failed, entered data is incorrect.",
+        422
+      );
+      console.error(validationError.statusCode);
+      res.status(validationError.statusCode).json({
+        message: validationError.message,
       });
+      return;
     }
-  
+
     const { title, content } = req.body;
-  
+
     // Create post in db
     const post = new Post({
       title: title,
       content: content,
-      imageUrl: 'images/googlepixel.png',
+      imageUrl: "images/googlepixel.png",
       creator: {
         name: "Abhianv",
       },
     });
-  
+
     const result = await post.save();
-  
+
     res.status(201).json({
       message: "Post created successfully.",
       post: result,
     });
-  } catch (error) {
-    console.log(error);
-    throw new Error("Failed to create a post.");
+  } catch (err) {
+    return next(new HttpError("Failed to create a post", 500));
   }
-  
 };
