bad30e82950d6a06c8b3fb3dd7d9dcfa0bbb8f01
Mr-Unforgettable <abhinavpsingh2024@protonmail.com>
2024-11-25 16:58:53 +0530

    Added a route to fetch single post

--- backend/routes/feed.ts
+++ backend/routes/feed.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPosts, createPost } from "../controllers/feed";
+import { getPosts, createPost, getPost } from "../controllers/feed";
 import { createPostsValidationRules } from "../validators/createPostsValidation";
 
 const router = express.Router();
@@ -9,5 +9,8 @@ router.get("/posts", getPosts);
 
 // POST => /feed/post
 router.post("/post", createPostsValidationRules(), createPost);
+
+// GET => /post/:postId
+router.get("/post/:postId", getPost);
 
 export default router;
