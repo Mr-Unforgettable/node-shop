4806aab82eea13cb0555d817237d8e31490c2a53
Mr-Unforgettable <abhinavpsingh2024@protonmail.com>
2024-11-25 14:27:41 +0530

    Implemented create post logic for create db query.

--- backend/controllers/feed.ts
+++ backend/controllers/feed.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import { validationResult } from "express-validator";
+import { Post } from "../models/post";
 
 export const getPosts: RequestHandler = (req, res, next) => {
   res.status(200).json({
@@ -18,30 +19,37 @@ export const getPosts: RequestHandler = (req, res, next) => {
   });
 };
 
-export const createPost: RequestHandler = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res
-      .status(422)
-      .json({
+export const createPost: RequestHandler = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(422).json({
         message: "Validation failed, entered data is incorrect.",
         errors: errors.array(),
       });
-  }
+    }
   
-  const { title, content } = req.body;
-
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
+    const { title, content } = req.body;
+  
+    // Create post in db
+    const post = new Post({
       title: title,
       content: content,
+      imageUrl: 'images/googlepixel.png',
       creator: {
         name: "Abhianv",
       },
-      createdAt: new Date(),
-    },
-  });
+    });
+  
+    const result = await post.save();
+  
+    res.status(201).json({
+      message: "Post created successfully.",
+      post: result,
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create a post.");
+  }
+  
 };
