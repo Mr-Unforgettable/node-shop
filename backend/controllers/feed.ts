81db6354ecd1071a24f998ea361ca1874d0a7651
Mr-Unforgettable <abhinavpsingh2024@protonmail.com>
2024-11-26 20:13:24 +0530

    Implement the file upload functionality.

--- backend/controllers/feed.ts
+++ backend/controllers/feed.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from "express";
 import { validationResult } from "express-validator";
 import { Post } from "../models/post";
+import path from "node:path";
+import fs from "node:fs/promises";
 
 // Custom HttpError class
 class HttpError extends Error {
@@ -20,10 +22,10 @@ export const getPosts: RequestHandler = async (req, res, next) => {
       res.status(200).json({
         message: "Fetched posts successfully.",
         posts: posts,
-      })
+      });
     }
   } catch (err) {
-    return next(new HttpError('Failed to fetch posts.', 500));
+    return next(new HttpError("Failed to fetch posts.", 500));
   }
 };
 
@@ -41,13 +43,22 @@ export const createPost: RequestHandler = async (req, res, next) => {
       return;
     }
 
+    if (!req.file) {
+      const validationError = new HttpError("No Image provided", 422);
+      res.status(validationError.statusCode).json({
+        message: validationError.message,
+      });
+      return;
+    }
+
+    const imageUrl = req.file.path;
     const { title, content } = req.body;
 
     // Create post in db
     const post = new Post({
       title: title,
       content: content,
-      imageUrl: "images/googlepixel.png",
+      imageUrl: imageUrl,
       creator: {
         name: "Abhianv",
       },
@@ -69,15 +80,70 @@ export const getPost: RequestHandler = async (req, res, next) => {
     const postId = req.params.postId;
     const getPost = await Post.findById(postId);
     if (!getPost) {
-      const error = new HttpError('Could not find post.', 404);
+      const error = new HttpError("Could not find post.", 404);
       throw error;
     }
 
     res.status(200).json({
       message: "Post fetched.",
       post: getPost,
-    })
+    });
   } catch (err) {
     return next(new HttpError("Failed to fetch a post", 500));
   }
 };
+
+export const updatePost: RequestHandler = async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationError = new HttpError(
+        "Validation failed, entered data is incorrect.",
+        422
+      );
+      res.status(validationError.statusCode).json({
+        message: validationError.message,
+      });
+      return;
+    }
+    const { title, content } = req.body;
+    let imageUrl = req.body.imageUrl;
+
+    if (req.file) {
+      imageUrl = req.file.path;
+    }
+    if (!imageUrl) {
+      const error = new HttpError("No file picked", 422);
+      throw error;
+    }
+
+    // Valid data so upload to db
+    const fetchedPost = await Post.findById(postId);
+    if (!fetchedPost) {
+      const error = new HttpError("Could not find post.", 404);
+      throw error;
+    }
+
+    if (imageUrl !== fetchedPost.imageUrl) {
+      clearImage(fetchedPost.imageUrl);
+    }
+    fetchedPost.title = title;
+    fetchedPost.imageUrl = imageUrl;
+    fetchedPost.content = content;
+
+    const result = await fetchedPost.save();
+    
+    res.status(200).json({ 
+      message: 'Post updated!',
+      post: result
+    });
+  } catch (err) {
+    return next(new HttpError("Failed to update a post", 500));
+  }
+};
+
+const clearImage = (filePath: string) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath);
+}
