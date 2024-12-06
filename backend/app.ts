75336cbda724f0af608265eede36aceb5ee14b71
Mr-Unforgettable <abhinavpsingh2024@protonmail.com>
2024-12-06 14:06:51 +0530

    Added auth route for the login and signup

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,11 @@
-import express from "express";
-import path from "node:path";
-import bodyParser from "body-parser";
-import feedRoutes from "./routes/feed";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import multer, { FileFilterCallback } from "multer";
+import express from 'express';
+import path from 'node:path';
+import bodyParser from 'body-parser';
+import feedRoutes from './routes/feed';
+import authRoutes from './routes/auth';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import multer, { FileFilterCallback } from 'multer';
 
 dotenv.config();
 
@@ -14,10 +15,10 @@ const PORT = 8080;
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "images");
+    callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    callback(null, new Date().toISOString() + "-" + file.originalname);
+    callback(null, new Date().toISOString() + '-' + file.originalname);
   },
 });
 
@@ -27,9 +28,9 @@ const fileFilter = (
   callback: FileFilterCallback
 ) => {
   if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
   ) {
     callback(null, true);
   } else {
@@ -40,20 +41,21 @@ const fileFilter = (
 const upload = multer({ storage: fileStorage, fileFilter: fileFilter });
 
 app.use(bodyParser.json()); // application/json
-app.use(upload.single("image"));
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(upload.single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-app.use("/feed", feedRoutes);
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error: any, req: any, res: any, next: any) => {
   // console.log(`Status Code: ${error}`);
--- backend/routes/feed.ts
+++ backend/routes/feed.ts
@@ -1,22 +1,31 @@
-import express from "express";
-import { getPosts, createPost, getPost, updatePost, deletePost } from "../controllers/feed";
-import { createPostsValidationRules, updatePostsValidationRules } from "../validators/postRouteValidation";
+import express from 'express';
+import {
+  getPosts,
+  createPost,
+  getPost,
+  updatePost,
+  deletePost,
+} from '../controllers/feed';
+import {
+  createPostsValidationRules,
+  updatePostsValidationRules,
+} from '../validators/postRouteValidation';
 
 const router = express.Router();
 
 // GET => /feed/posts
-router.get("/posts", getPosts);
+router.get('/posts', getPosts);
 
 // POST => /feed/post
-router.post("/post", createPostsValidationRules(), createPost);
+router.post('/post', createPostsValidationRules(), createPost);
 
 // GET => /post/:postId
-router.get("/post/:postId", getPost);
+router.get('/post/:postId', getPost);
 
 // PUT => /post/:postId
-router.put("/post/:postId", updatePostsValidationRules(), updatePost);
+router.put('/post/:postId', updatePostsValidationRules(), updatePost);
 
 // DELETE => /post/:postId
-router.delete("/post/:postId", deletePost);
+router.delete('/post/:postId', deletePost);
 
 export default router;
